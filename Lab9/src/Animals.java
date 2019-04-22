1a851154d362e3bb385b76c24f1de87f3ef0d772
natalyaalansi <49843485+natalyaalansi@users.noreply.github.com>
2019-04-23 02:05:15 +0300

    Labs JavaSE

---
+++ Lab9/src/Animals.java
@@ -0,0 +1,3 @@
+public abstract class Animals {
+    public void voice () {}
+}
---
+++ Lab9/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        new Doge().voice();
+        new Doge().playWithBall();
+        new AssistanceDog().voice();
+        new AssistanceDog().playWithBall();
+        new AssistanceDog().train();
+        new SpaceDog().voice();
+        new SpaceDog().playWithBall();
+        new SpaceDog().train();
+        new SpaceDog().askWhy();
+
+    }
+}
